3177910923e6b0f015850c50ccb9cb246ed75ec9
philipbenn <115698222+philipbenn@users.noreply.github.com>
2023-04-24 10:47:32 +0200

    Startup

---
+++ BlomBiler/src/main/java/com/example/blombiler/BlomBilerApplication.java
@@ -0,0 +1,13 @@
+package com.example.blombiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlomBilerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlomBilerApplication.class, args);
+    }
+
+}
---
+++ BlomBiler/src/test/java/com/example/blombiler/BlomBilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blombiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlomBilerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
