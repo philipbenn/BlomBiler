e50fbd9a04467af932820710818f6260cc0f8eef
philipbenn <phil658c@stud.kea.dk>
2023-04-25 11:02:04 +0200

    login funktion og index html design :D

--- BlomBiler/src/main/java/com/example/blombiler/controller/CustomerController.java
+++ BlomBiler/src/main/java/com/example/blombiler/controller/CustomerController.java
@@ -5,9 +5,11 @@ import com.example.blombiler.model.Customer;
 import com.example.blombiler.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CustomerController {
@@ -17,7 +19,7 @@ public class CustomerController {
 
     @GetMapping("/")
     public String signUpPage(){
-        return "signup";
+        return "index";
     }
 
 
@@ -27,8 +29,25 @@ public class CustomerController {
         return "redirect:/showcars";
     }
 
+    @PostMapping("/login")
+    public String logIn(@RequestParam String customer_email, @RequestParam String customer_password, Model model) {
+        Customer customer = customerService.logIn(customer_email, customer_password);
+        if (customer != null) {
+            model.addAttribute("customer", customer);
+            return "redirect:/showcars";
+        } else {
+            model.addAttribute("error", "Invalid email or password");
+            return "login";
+        }
+    }
 
+    @GetMapping("/loginForm")
+    public String showLoginForm() {
+        return "login";
+    }
 
-
-
+    @GetMapping("/signUpForm")
+    public String showSignUpForm() {
+        return "signup";
+    }
 }
--- BlomBiler/src/main/java/com/example/blombiler/repository/CustomerRepo.java
+++ BlomBiler/src/main/java/com/example/blombiler/repository/CustomerRepo.java
@@ -2,7 +2,10 @@ package com.example.blombiler.repository;
 
 import com.example.blombiler.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,10 +13,20 @@ public class CustomerRepo {
     @Autowired
     JdbcTemplate template;
 
-
     public void addCustomerAccount(Customer customer){
         String sql = "INSERT INTO customer (customer_id, customer_first_name, customer_last_name, customer_password, customer_email) VALUES (?, ?, ?, ?, ?)";
         template.update(sql, customer.getCustomer_id(), customer.getCustomer_first_name(), customer.getCustomer_last_name(), customer.getCustomer_password(), customer.getCustomer_email());
+    }
+
+    public Customer logIn(String customer_email, String customer_password) {
+        String sql = "SELECT * FROM customer WHERE customer_email = ? AND customer_password = ?";
+        try {
+            RowMapper<Customer> rowMapper = new BeanPropertyRowMapper<>(Customer.class);
+            Customer customer = template.queryForObject(sql,rowMapper, customer_email, customer_password);
+            return customer;
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
 }
